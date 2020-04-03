94e5c4ce0f0cc3b1f3990d03a774d06e8a3116fc
olfusik15 <olfusik15@gmail.com>
2020-04-03 17:43:32 +0200

    Add price compare in two markets

--- L3_coin.py
+++ L3_coin.py
@@ -4,14 +4,32 @@ def bitbay_orders():
     r = requests.get("https://bitbay.net/API/Public/BTCPLN/orderbook.json")
     return r.json()
 
-orders = bitbay_orders()
-bids = orders["bids"]
-asks = orders["asks"]
+def bitbay_ticker():
+    r = requests.get("https://bitbay.net/API/Public/BTCPLN/ticker.json")
+    return r.json()
+
+def blockchain_ticker():
+    r = requests.get("https://blockchain.info/ticker")
+    return r.json()
+
+bitbay = bitbay_orders()
+bb_ticker = bitbay_ticker()
+bc_ticker = blockchain_ticker()
 
 print("Oferty kupna:")
-for i in bids[:10]:
+for i in bitbay["bids"][:10]:
     print(i)
 
 print("\n Oferty sprzedaży: ")
-for i in asks[:10]:
+for i in bitbay["asks"][:10]:
     print(i)
+
+if bb_ticker["bid"]>bc_ticker["PLN"]["buy"]:
+    print("Buy price is better on bitbay.net: ", bb_ticker["bid"])
+else:
+        print("Buy price is better on blockchain.info:" , bc_ticker["PLN"]["buy"])
+
+if bb_ticker["ask"]>bc_ticker["PLN"]["sell"]:
+    print("Sell price is better on bitbay.net: " , bb_ticker["ask"])
+else:
+        print("Sell price is better on blockchain.info: " , bc_ticker["PLN"]["sell"])
